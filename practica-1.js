72425f0a34550d1c442e9a75d5aaabee19bf2135
Carlos <karlosssanta@gmail.com>
2020-02-12 13:09:01 +0100

    first javascript 'program'

---
+++ practica-1.js
@@ -0,0 +1,6 @@
+var nombre = "Carlos";
+var apellidos = "Santamaría Simón";
+var edad = 22;
+var weekDays = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+var gravedad = 9.81;
+var impares = [1, 3, 5];
